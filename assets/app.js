53822216574eca745fc125476b1fa8c5a55b2f28
brarver <brarver@yahoo.com>
2019-07-29 12:13:08 -0600

    players finished, working on game display

--- assets/app.js
+++ assets/app.js
@@ -12,22 +12,184 @@ firebase.initializeApp(firebaseConfig);
 
 var database = firebase.database();
 
-var player1 = {
-    guess: null,
-    score: null,
-}
+var game = database.ref('/game')
+var players = database.ref('/players')
 
-var player2 = {
-    guess: null,
-    score: null,
-}
-
+var player1
+var player2
+var you
 var ties = null
-var p1s = player1.score
-var p2s = player2.score
-var p1g = player1.guess
-var p2g = player2.guess
+var p1s = null
+var p2s = null
+var p1g = null
+var p2g = null
 
+//////////////////////////////////////Connections////////////////////////////////////////////////////////////////
+
+var connectionsRef = database.ref("/connections");
+
+var connectedRef = database.ref(".info/connected");
+
+
+connectedRef.on("value", function(snap) {
+
+  
+  if (snap.val()) {
+
+    var con = connectionsRef.push(true);
+    
+    con.onDisconnect().remove();
+
+  }
+});
+
+connectionsRef.on("value", function(snap) {
+
+    console.log(snap.numChildren())
+
+  $("#connected-viewers").text(snap.numChildren());
+});
+
+
+/////////////////////////////////////set players//////////////////////////////////////////////
+
+$('#add-player').on('click', function() {
+
+    if (!player1) {
+        player1 = $('#player-name').val().trim()
+        $('.info').empty()
+        $('.info').text(player1 + ' your are player 1, waiting on player 2')
+        you = 'one'
+        players.set({
+            player1: player1
+        })
+
+    } else if (!player2) {
+        player2 = $('#player-name').val().trim()
+        $('.info').empty()
+        $('.info').text(player2 + ' your are player 2')
+        you = 'two'
+        players.set({
+            player1: player1,
+            player2: player2
+        })
+    }
+})
+
+players.on('value', function(snapshot) {
+
+    if (snapshot.exists()) {
+        player1 = snapshot.val().player1
+        player2 = snapshot.val().player2
+        $('#player-1-name').text(player1)
+        $('#player-2-name').text(player2)
+    }
+
+    if (snapshot.child('player2').exists()) {
+        console.log('bear')
+        renderOne()
+    }
+    
+})
+
+/////////////////////////////////////////game//////////////////////////////////////////////////////////////////
+
+function renderOne() {
+    var items = ['rock', 'paper', 'scissors']
+
+    if (you === 'one') {
+        for (var i = 0; i < items.length; i++) {
+            var div = $('<div>').addClass('item p1b')
+            div.text(items[i])
+            $('.player1').append(div)
+        }
+        playerOneChoose()
+    }
+
+    var p = $('<p>').text('waiting on player 1')
+    $('.player2').append(p)
+    
+}
+
+function renderTwo() {
+    var items = ['rock', 'paper', 'scissors']
+
+    if (you === 'two') {
+        $('.player2').empty()
+        console.log('seal')
+        for (var i = 0; i < items.length; i++) {
+            var div = $('<div>').addClass('item p2b')
+            div.text(items[i])
+            $('.player2').append(div)
+        }
+        playerTwoChoose()
+    }
+    
+    var p = $('<p>').text('waiting on player 2')
+    $('.player1').append(p)
+    
+}
+
+game.on("value", function (snapshot) {
+
+    if (snapshot.child('playerOneGuess').exists()) {
+        p1g = snapshot.val().playerOneGuess  
+        renderTwo()
+    }
+    if (snapshot.child('playerTwoGuess').exists()) {
+        p2g = snapshot.val().playerTwoGuess  
+    }
+    if (snapshot.child('playerOneScore').exists()) {
+        p1s = snapshot.val().playerOneScore
+    }
+    if (snapshot.child('playerTwoScore').exists()) {
+        p2s = snapshot.val().playerTwoScore
+    }
+    if (snapshot.child('ties').exists()) {
+        ties = snapshot.val().ties
+    }
+
+    if (snapshot.child('playerOneGuess').exists() && snapshot.child('playerTwoGuess').exists()) {
+        return play(snapshot.val().playerOneGuess, snapshot.val().playerTwoGuess)
+    }
+})
+
+function playerOneChoose() {
+    console.log('deer')
+
+    $('.p1b').on('click', (e) => {
+        p1g = e.target.innerText
+        console.log('player 1: ' + p1g)
+        game.set({
+            playerOneGuess: p1g,
+            playerTwoGuess: p2g,
+            playerOneScore: p1s,
+            playerTwoScore: p2s,
+            ties: ties
+        })
+    })    
+}
+
+function playerTwoChoose() {
+    console.log('elk')
+
+    $('.p2b').on('click', (e) => {
+        p2g = e.target.innerText
+        console.log('player 2: ' + p2g)
+        game.set({
+            playerOneGuess: p1g,
+            playerTwoGuess: p2g,
+            playerOneScore: p1s,
+            playerTwoScore: p2s,
+            ties: ties
+        })
+        renderOne()
+    })
+}
+
+
+
+///////////////////////////////Game play////////////////////////////////////////////////////
 function play(a, b) {
     if (a === b) {
         return tie()
@@ -56,15 +218,14 @@ function play(a, b) {
             return twoWins()
         }
     }
-
 }
 
 function oneWins() {
     p1s++
     p1g = null
     p2g = null
-    console.log('player one wins')
-    database.ref().set({
+    $('.board').text('Player One Wins!')
+    game.set({
         playerOneGuess: p1g,
         playerTwoGuess: p2g,
         playerOneScore: p1s,
@@ -78,8 +239,8 @@ function twoWins() {
     p2s++
     p1g = null
     p2g = null
-    console.log('player two wins')
-    database.ref().set({
+    $('.board').text('Player Two Wins!')
+    game.set({
         playerOneGuess: p1g,
         playerTwoGuess: p2g,
         playerOneScore: p1s,
@@ -92,8 +253,8 @@ function tie() {
     ties++
     p1g = null
     p2g = null
-    console.log('there was a tie')
-    database.ref().set({
+    $('.board').text('There was a tie')
+    game.set({
         playerOneGuess: p1g,
         playerTwoGuess: p2g,
         playerOneScore: p1s,
@@ -102,55 +263,6 @@ function tie() {
     })
 
 }
-
-database.ref().on("value", function (snapshot) {
-
-    if (snapshot.child('playerOneGuess').exists()) {
-        p1g = snapshot.val().playerOneGuess  
-    }
-    if (snapshot.child('playerTwoGuess').exists()) {
-        p2g = snapshot.val().playerTwoGuess  
-    }
-    if (snapshot.child('playerOneScore').exists()) {
-        p1s = snapshot.val().playerOneScore
-    }
-    if (snapshot.child('playerTwoScore').exists()) {
-        p2s = snapshot.val().playerTwoScore
-    }
-    if (snapshot.child('ties').exists()) {
-        ties = snapshot.val().ties
-    }
-
-    if (snapshot.child('playerOneGuess').exists() && snapshot.child('playerTwoGuess').exists()) {
-        return play(snapshot.val().playerOneGuess, snapshot.val().playerTwoGuess)
-    }
-})
-
-
-
-$('.p1b').on('click', (e) => {
-    p1g = e.target.innerText
-    console.log('player 1: ' + p1g)
-    database.ref().set({
-        playerOneGuess: p1g,
-        playerTwoGuess: p2g,
-        playerOneScore: p1s,
-        playerTwoScore: p2s,
-        ties: ties
-    })
-})
-
-$('.p2b').on('click', (e) => {
-    p2g = e.target.innerText
-    console.log('player 2: ' + p2g)
-    database.ref().set({
-        playerOneGuess: p1g,
-        playerTwoGuess: p2g,
-        playerOneScore: p1s,
-        playerTwoScore: p2s,
-        ties: ties
-    })
-})
 
 //when player makes choice set their decision
 //if there are two guesses playgame() inside of global data listener
